3cc592d4901c3ec33580ae41f95fe2354d3ab5cd
zdqlwe <1440785141@qq.com>
2024-11-26 16:59:08 +0800

    Site updated: 2024-11-26 16:59:07

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732611441587";
+const VERSION = "1732611542021";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
